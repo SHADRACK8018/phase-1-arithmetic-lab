33816964a82f8e2793a5b606758273f6dceb20ca
Shadrack <skiplagat8018@gmail.com>
2024-11-27 18:59:20 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 100) + 1;
+let num3 = 19;
+let num4 = 5;
+
+let mod = num3 % num4;
+
+let values = [12, 5, 20, 3];
+let max = Math.max(...values);
+
+console.log("Multiply:", multiply);
+console.log("Random:", random);
+console.log("Mod:", mod);
+console.log("Max:", max);
